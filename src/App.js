0a2f5c0e2b5bd6d23225e2a13b9661192d1dbbbd
Rakesh Sankhla <75526736+rsankhla20@users.noreply.github.com>
2023-08-22 04:18:46 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import Layout from "./components/Layout/Layout";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import Education from "./pages/Education/Education";
+import Projects from "./pages/Projects/Projects";
+import Teckstack from "./pages/Teckstack/Teckstack";
+import WorkExp from "./pages/WorkExp/WorkExp";
+import ScrollToTop from "react-scroll-to-top";
+import { useTheme } from "./context/ThemeContext";
+import Tada from "react-reveal/Tada";
+import MobNav from "./components/MobNav/MobNav";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const [theme] = useTheme();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id={theme}>
+        <ToastContainer />
+        <MobNav />
+        <Layout />
+        <div className="container">
+          <About />
+          <Education />
+          <Teckstack />
+          <Projects />
+          {/* <WorkExp /> */}
+          <Contact />
+        </div>
+        <div className="footer pb-3 ms-3">
+          <Tada>
+            <h4 className="text-center">Made With ❤️ By Rakesh &copy; 2023</h4>
+          </Tada>
+        </div>
+      </div>
+      <ScrollToTop
+        smooth
+        color="white"
+        style={{ backgroundColor: "#001f3f", borderRadius: "80px" }}
+      />
+    </>
   );
 }
 
